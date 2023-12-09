e2404a12b68fa0c09616c42d01e7c8063178b5e0
Derevjannykh Ivan <ivan.der.sarapul@gmail.com>
2023-12-09 21:15:35 +0400

    refactor: burger menu markup page home

--- coffee-house/pages/home/main.js
+++ coffee-house/pages/home/main.js
@@ -1,11 +1,18 @@
 const menuBtn = document.querySelector('.burger__btn');
+const menuMobile = document.querySelector('.nav__menu');
+const navItem = menuMobile.querySelectorAll('.nav__menu_item');
+const body = document.body;
 
 menuBtn.addEventListener('click',()=> {
-    menuBtn.classList.toggle('burger--transform');	
+    menuBtn.classList.toggle('burger--transform');
+	menuMobile.classList.toggle('burger--open');
+	body.classList.toggle('scroll--stop')	
 });
 
 navItem.forEach(element => {
 	element.addEventListener("click",()=> {
 		menuBtn.classList.remove('burger--transform');
+		menuMobile.classList.remove('burger--open');
+		body.classList.remove('scroll--stop')
 	});
 });
